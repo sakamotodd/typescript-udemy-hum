c33bce2b4e4ec4cef82269a59f5dd728b5e918fa
sakamotodd <73228728+sakamotodd@users.noreply.github.com>
2021-06-26 12:50:39 +0900

    ts-node-devのパッケージ追加

--- src/install-typescript.js
+++
@@ -1,2 +0,0 @@
-var message = 'Hello would!';
-console.log({ message: message });
--- src/install-typescript.ts
+++ src/install-typescript.ts
@@ -1,2 +1,2 @@
-let message: string = 'Hello would!'
+let message: string = 'Hello would! + sakammotodaigoro'
 console.log({message})
