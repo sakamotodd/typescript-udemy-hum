530ac58e77908541d9ab1ed9bb378514c4d62ce0
sakamotodd <73228728+sakamotodd@users.noreply.github.com>
2021-07-04 15:37:34 +0900

    📝 Typescript-関数-勉強

--- src/function/allow-function.ts
+++ src/function/allow-function.ts
@@ -10,3 +10,12 @@ let bmi: (wei: number, hei: number) => number = (
 };
 
 console.log(bmi(1.71, 62));
+
+//省略した書き方
+//アクセシビリティ(public, protected, private)の修飾子
+
+const all = (age: number): number => {
+  return age
+}
+
+console.log(all(23))
